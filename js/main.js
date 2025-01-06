29a062befa1aec417be7caafac83acb65de2f364
Leman123-hub <lemanesgerova3899@gmail.com>
2025-01-06 22:19:49 +0400

    qiymetlendirme

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let qiymet = 5;
+switch (qiymet) {
+    case 2:
+        document.write("QEYRI-KAFI");
+        break;
+    case 3:
+        document.write("KAFI");
+        break;
+    case 4:
+        document.write("YAXWI");
+        break;
+    case 5:
+        document.write("ELA");
+        break;
+    default:
+        document.write("Qiymet Duzgun Daxil Edilmeyib!");
+
+}
